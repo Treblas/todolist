41b6d011a58b176afc8612b2ed17d4a4aad407c3
Trebla <salvajamesalbert@gmail.com>
2017-12-07 17:59:42 +0800

    todoss

--- src/App.js
+++ src/App.js
@@ -1,43 +1,101 @@
 import React, { Component } from 'react';
 import Todoform from './Components/Todoform'
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
    state = {
-     todo_empty: '',
-     todos: [],
-     details: {}
+      search: '',
+      details: {},
+      notes: [],
+      note: '',
+      todoid: 1,
+      status: 'Active',
+      counter: 0,
+      countAll: 0
+
   }
-  handleChange(prop, e){
+   handleChange(prop, e){
     this.setState({[prop]: e.target.value })
+  }
+  
+  removeTodo(name, i){
+    this.setState({
+      counter: this.state.counter - 1
+    })
+    let notes = this.state.notes.slice();
+    notes.splice(i, 1);
+    this.setState({
+      notes
+    })
   }
   handleSave(){
       var obj ={
-        id:   this.state.details.id,
-        todos: this.state.todo_empty
+       //id:   this.state.details.id,
+        note: this.state.note,
+        todoid: this.state.todoid,
+        status: this.state.status,
+        counter: this.state.counter + 1
+
       }
       this.setState({
-       todos: this.state.todos.concat(obj),
-       todo_empty: ''})
+       notes: this.state.notes.concat(obj),
+       note: '',
+       todoid: this.state.todoid + 1,
+       status: this.state.status,
+       counter:  this.state.counter + 1 
+     })
+     //const {details, note, notes} = this.state
+
   }
 
- 
+  handleCompleted(){
+    
+     var complete = 'Completed'
+     var completeobj ={
+      counter: this.state.counter ,
+      status: this.state.status
+    }
+    this.setState({
+       counter:  this.state.counter - 1 ,
+      notes: this.state.notes = 'Completed'
+      // status: ({this.state.status: , 'Completed'})
+
+
+     })
+  }
+  handleRemove(){
+    var todolist = this.state.notes;
+    var removeobj ={
+      counter: this.state.counter 
+
+    }
+    this.setState({
+       counter:  this.state.counter - 1 
+     })
+  }
   render() {
+      console.log('Todo List :', this.state.notes)
+      console.log('Counter :', this.state.counter)
+
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">My Todo List</h1>
            <Todoform
-             handleChange={this.handleChange.bind(this, 'todo_empty')}
-             handleSave={this.handleSave.bind(this)} 
-             todos={this.state.todos}
-             details={this.state.details}
-             todo_empty={this.state.todo_empty} />
-        </header>
-       
+               handleChange={this.handleChange.bind(this, 'note')}
+               handleSave={this.handleSave.bind(this)}
+               notes={this.state.notes}
+               details={this.state.details}
+               note={this.state.note}
+               counter={this.state.counter}
+               handleRemove={this.handleRemove.bind(this, 'counter')}
+               handleCompleted={this.handleCompleted.bind(this)}
+               removeTodo={this.removeTodo.bind(this, 'counter')}
+               id={this.state.todoid}
+           />
+
+        </header> 
       </div>
     );
   }
--- src/Components/Todoform.js
+++ src/Components/Todoform.js
@@ -1,36 +1,62 @@
 import React, { Component } from 'react';
-import request from 'superagent';
 import '../App.css';
  
  class Todoform extends Component {
-  const {details, todos} = this.props
-  const todoList = todos
- .filter((todoList) => {
-    return details.id == todoList.id 
-    })
- console.log(todoList)
    render() {
+    const { details, notes , counter, todoid} = this.props
+    console.log(details)
+    const note = notes
+    .filter((note) => {
+      return details.id === note.id })
+    .map((msg) => <div className="todoitem" 
+                       /*key="note"*/
+                       id={msg.todoid}>
+                       <input className="checkbox"
+                       type="checkbox">
+                       </input>
+                       <div className="noteitem">
+                         {msg.note}</div>
+                       <button className="Completebtn"
+                                onClick={this.props.handleCompleted.bind(this, msg)}
+                               >Completed </button>
+                       <button className="Removebtn"
+                              onClick={this.props.removeTodo}
+                              >Remove
+                        </button>
+                  </div>)
     return (
       <div className="Todoform">
-        <input
-          className="Inputbox"
-          type="text"
-          placeholder="Text here..."
-          onChange={this.props.handleChange}
-          value={this.props.todo_empty}>
-        </input>
-      <button 
-          className="ButtonAdd"
-           onClick={this.props.handleSave}>
-           + Add Note
-      </button>
-      {
-        todoList.map(item,index) =>{
-        return <h2 key={index}>{item.todos}</h2>
-        })
-       }
-      </div>
+      <div className="MainHeader">
+            <h2 className="MainTitle">TO DO LIST  </h2>
+         </div>
+        <div className="FormContainer">
+         <input
+           className="Inputbox"
+           type="text"
+           placeholder="Text here..."
+           onChange={this.props.handleChange}>
+         </input>
+         <button 
+           className="ButtonAdd"
+           onClick={this.props.handleSave}> + Add Note
+         </button>
+        </div>
+        <br/>
+        
 
+        <div className="NoteContainer">
+          <div className="todonote">{note}</div>
+        </div>
+        <div className="StatusContainer">
+         <label className="Stat">{this.props.counter} todo/s left</label>
+         <label className="allStat">All</label>
+         <label className="activeStat">Active</label>
+         <label className="completedStat">Completed</label>
+
+
+        </div>
+         <br/>
+      </div>
     );
   }
 } 
